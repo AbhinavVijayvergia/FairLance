0965251c5d1e8934ed49f792c555202d395ddc84
Abhinav Vijayvergia <64889289+AbhinavVijayvergia@users.noreply.github.com>
2026-03-14 15:50:08 +0530

    Update ai_agent.py

--- API/ai_agent.py
+++ API/ai_agent.py
@@ -5,9 +5,7 @@ from groq import Groq
 # CONFIGURATION
 # ============================================================
 
-client = Groq(api_key="
-# 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
-")
+client = Groq(api_key="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")
 
 MODEL = "llama-3.3-70b-versatile"
 
